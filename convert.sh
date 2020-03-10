b89eac9eb1e4aef207fa63721c8d758de2a15142
Jordan Matelsky <j6k4m8@gmail.com>
2020-03-09 21:43:34 -0400

    Remove old files

--- convert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/bin /usr/local/bin/pandoc -r html daily.html -V fontsize=12pt -V geometry:"top=2cm, bottom=1.5cm, left=5cm, right=1cm" --latex-engine=/Library/TeX/texbin/xelatex -o "daily.pdf"
