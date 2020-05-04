de322cb025700c9f5ba85327e15f9181ca99c481
Jordan Matelsky <j6k4m8@gmail.com>
2020-05-03 21:53:25 -0400

    Update frof

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-from typing import List
-import feedparser
-from goosepaper.styles import Styles
-
-
-FEEDS = [
-    feedparser.parse(feed)
-    for feed in [
-        "https://morss.it/https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
-        "https://morss.it/feeds.reuters.com/reuters/topNews.rss",
-        "https://morss.it/feeds.bbci.co.uk/news/rss.xml",
-    ]
-]
-
-entries = []
-for feed in FEEDS:
-    entries = [*entries, *feed.entries]
-
-html_entries = []
-
-for entry in entries:
-    if "content" not in entry:
-        continue
-    html_entries.append(
-        f"""
-    <article>
-        <h1>{entry['title']}</h1>
-        <h4>{entry['published']}</h4>
-        {entry['content'][0]['value']}
-    </article>
-    """
-    )
-
-style = Styles.Autumn
+from goosepaper import Goosepaper, WikipediaCurrentEventsStoryProvider, TwitterStoryProvider, LoremStoryProvider, WeatherStoryProvider
 
 print(
-    "<html><head><style>"
-    + style
-    + "</style></head><body>"
-    + "<hr />".join(html_entries)
-    + "</body></html>"
+    Goosepaper([
+        WikipediaCurrentEventsStoryProvider(),
+        WeatherStoryProvider(woe="2358820"),
+        TwitterStoryProvider("reuters", limit=5),
+        TwitterStoryProvider("bbcWorld", limit=5),
+    ]).to_html()
 )
