8ea0e6195dd4319c34e528f24177900bb6495d5d
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2020-10-12 19:25:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ print(
             WeatherStoryProvider(woe="2358820"),
             RSSFeedStoryProvider("https://www.statnews.com/feed/", limit=5),
             RSSFeedStoryProvider("https://www.npr.org/feed/", limit=5),
-            MultiTwitterStoryProvider(
-                ["reuters", "bbcWorld", "axios", "BethanyAllenEbr", "NPR"], limit_per=5
-            ),
+            #MultiTwitterStoryProvider(
+            #    ["reuters", "bbcWorld", "axios", "BethanyAllenEbr", "NPR"], limit_per=5
+            #),
+            # Pending this issue: https://github.com/j6k4m8/goosepaper/issues/5
         ]
     ).to_html()
 )
