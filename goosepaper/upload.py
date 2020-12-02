09fe212c848eeca1883e181c4b160210b973b608
Jordan Matelsky <j6k4m8@gmail.com>
2020-12-01 20:57:37 -0500

    Lint upload file

--- goosepaper/upload.py
+++ goosepaper/upload.py
@@ -9,16 +9,13 @@ from rmapy.exceptions import AuthError
 def upload(filepath=None):
 
     if not filepath:
-        parser = argparse.ArgumentParser(
-            "Upload Goosepaper to reMarkable tablet"
-        )
+        parser = argparse.ArgumentParser("Upload Goosepaper to reMarkable tablet")
         parser.add_argument(
-            "file",
-            default=None,
-            help="The file to upload",
+            "file", default=None, help="The file to upload",
         )
         args = parser.parse_args()
         filepath = args.file
+
     filepath = Path(filepath)
 
     client = Client()
@@ -27,7 +24,8 @@ def upload(filepath=None):
         client.renew_token()
     except AuthError:
         print(
-            "Looks like this if the first time you've uploaded, need to register the device")
+            "Looks like this if the first time you've uploaded, need to register the device"
+        )
         print("Get the code from here: https://my.remarkable.com/connect/remarkable")
         code = input()
         print("registering")
@@ -40,7 +38,7 @@ def upload(filepath=None):
     for item in client.get_meta_items():
         if item.VissibleName == filepath.stem:
             print("Honk! Paper already exists!")
-            return True
+            return False
 
     doc = ZipDocument(doc=str(filepath.resolve()))
     if client.upload(doc):
