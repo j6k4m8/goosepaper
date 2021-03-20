100e03639273d2a3baf7584b74a527f5fbf1712f
Danilo Luvizotto <danilo.luvizotto@gmail.com>
2021-03-21 00:23:58 +0100

    Parallelize fetching RSS data

--- goosepaper/rss.py
+++ goosepaper/rss.py
@@ -2,6 +2,7 @@ import requests
 import feedparser
 from typing import List
 from readability import Document
+import multiprocessing
 
 from .story import Story
 from .util import PlacementPreference
@@ -16,19 +17,21 @@ class RSSFeedStoryProvider(StoryProvider):
     def get_stories(self, limit: int = 5) -> List[Story]:
         feed = feedparser.parse(self.feed_url)
         limit = min(self.limit, len(feed.entries))
-        stories = []
 
-        for entry in feed.entries[:limit]:
-            if "link" in entry.keys():
-                req = requests.get(entry["link"])
-                if not req.ok:
-                    print(f"Honk! Couldn't grab content for {self.feed_url}")
-                    continue
+        with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+            stories = pool.map(self.parallelizable_request, feed.entries)
 
-                doc = Document(req.content)
-                source = entry["link"].split(".")[1]
-                stories.append(
-                    Story(doc.title(), body_html=doc.summary(), byline=source)
-                )
+        return list(filter(None, stories))
 
-        return stories
+    def parallelizable_request(self, entry):
+        req = requests.get(entry["link"])
+        if not req.ok:
+            print(f"Honk! Couldn't grab content for {self.feed_url}")
+            return None
+
+        doc = Document(req.content)
+        source = entry["link"].split(".")[1]
+        story = Story(doc.title(), body_html=doc.summary(), byline=source) 
+
+        return story
+
