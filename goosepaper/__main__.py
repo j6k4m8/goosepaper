3df7f3933551e991fa706598223de1b8bd491b62
Danilo Luvizotto <danilo.luvizotto@gmail.com>
2021-03-24 14:37:01 -0300

    Add multiparser class (#37)

--- goosepaper/__main__.py
+++ goosepaper/__main__.py
@@ -1,73 +1,25 @@
 import sys
-import argparse
 import datetime
 
 from goosepaper.goosepaper import Goosepaper
-from goosepaper.util import (
-    construct_story_providers_from_config_dict,
-    load_config_file,
-)
+from goosepaper.util import construct_story_providers_from_config_dict
 from goosepaper.upload import upload
+from goosepaper.multiparser import MultiParser
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog="goosepaper",
-        description="Goosepaper generates and delivers a daily newspaper in PDF format."
-    )
-    parser.add_argument(
-        "-c",
-        "--config",
-        required=False,
-        default=None,
-        help="The json file to use to generate this paper.",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        required=False,
-        help="The output file path at which to save the paper",
-    )
-    parser.add_argument(
-        "-u",
-        "--upload",
-        action="store_true",
-        required=False,
-        help="Whether to upload the file to your remarkable using rmapy.",
-    )
-    parser.add_argument(
-        "--replace",
-        action="store_true",
-        required=False,
-        help="Will replace a document with same name in your remarkable.",
-    )
-
-    args = parser.parse_args()
-
-    try:
-        config = load_config_file(args.config)
-    except FileNotFoundError as e:
-        raise FileNotFoundError(
-            f"Could not find the configuration file at {args.config}"
-        ) from e
+    multiparser = MultiParser()
+    config = multiparser.config
 
     story_providers = construct_story_providers_from_config_dict(config)
+
     title = config["title"] if "title" in config else None
     subtitle = config["subtitle"] if "subtitle" in config else None
-    if args.output:
-        filename = args.output
-    elif "filename" in config:
-           filename = config["filename"]
-    else:
-        filename = f"Goosepaper-{datetime.datetime.now().strftime('%Y-%B-%d-%H-%M')}.pdf"
-    if args.replace:
-        if not args.upload:
-            parser.error("--replace requires --upload.")
-        replace = args.replace
-    elif "replace" in config:
-        replace = config["replace"]
-    else:
-        replace = False
+    filename = multiparser.argumentOrConfig(
+        "output",
+        default=f"Goosepaper-{datetime.datetime.now().strftime('%Y-%B-%d-%H-%M')}.pdf"
+    )
+    replace = multiparser.argumentOrConfig("replace", False)
 
     paper = Goosepaper(story_providers=story_providers, title=title, subtitle=subtitle)
 
@@ -81,11 +33,10 @@ def main():
     else:
         raise ValueError(f"Unknown file extension '{filename.split('.')[-1]}'.")
 
-    if args.upload:
+    if multiparser.argumentOrConfig("upload"):
         upload(filename, replace)
 
     return 0
-
 
 if __name__ == "__main__":
     sys.exit(main())
