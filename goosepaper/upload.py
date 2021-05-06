21f6339a84a1b208df3ac177848c4795a817b114
Sedennial <korwyn@protonmail.com>
2021-05-06 13:48:44 -0700

    Moved auth functions from upload into a new module.

--- goosepaper/upload.py
+++ goosepaper/upload.py
@@ -1,9 +1,10 @@
+from rmapy.document import ZipDocument
+from rmapy.api import Client, Document, Folder
+
 import argparse
 from pathlib import Path
 
-from rmapy.document import ZipDocument
-from rmapy.api import Client, Document, Folder
-from rmapy.exceptions import AuthError
+from .auth import auth_client
 
 def validateFolder(folder: str):
     if folder:
@@ -29,22 +30,10 @@ def upload(filepath=None, replace=False, folder=None):
 
     filepath = Path(filepath)
 
-    client = Client()
-
-    try:
-        client.renew_token()
-    except AuthError:
-        print(
-            "Looks like this if the first time you've uploaded, need to register the device"
-        )
-        print("Get the code from here: https://my.remarkable.com/connect/remarkable")
-        code = input()
-        print("registering")
-        client.register_device(code)
-        if not client.renew_token():
-            print("registration failed D:")
-        else:
-            print("registration successful")
+    client = auth_client()
+    if not client:
+        print ("Honk Honk! Couldn't auth! Is your rmapy configured?")
+        return False
 
     if not validateFolder(folder):
         return False
@@ -113,4 +102,3 @@ def upload(filepath=None, replace=False, folder=None):
     else:
         print("Honk! Could not upload: Document already exists.")
     return False
-
