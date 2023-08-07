0785f327ff58bb8f0173997f307b6a4e9a286c09
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2023-08-07 14:51:53 -0400

    feat: Add mastodon SP, and fix RSS provider (#82)
    * feat: Add mastodon SP, and fix RSS provider
    
    * lint: Remove unused imports

---
+++ goosepaper/storyprovider/test_mastodon.py
@@ -0,0 +1,13 @@
+# neuromatch.social/@jordan.rss
+
+from .mastodon import MastodonStoryProvider
+
+
+def test_can_get_mastodon_stories():
+    LIMIT = 5
+    stories = MastodonStoryProvider(
+        "https://neuromatch.social",
+        "jordan",
+        limit=LIMIT,
+    ).get_stories()
+    assert len(stories) == LIMIT
